872dd55cdcf027c20e220c13ead2e0548b379e8c
Gary <hai.alphacreative@gmail.com>
2025-04-08 10:36:55 +0700

    add animation load image

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -2,8 +2,25 @@ import { preloadImages } from "../../libs/utils.js";
 let lenis;
 Splitting();
 
+function animationMaskCentral() {
+  const animationImages = document.querySelectorAll(".animation-image");
+
+  animationImages.forEach((image) => {
+    gsap.to(image, {
+      scrollTrigger: {
+        trigger: image,
+        start: "top 50%",
+        end: "bottom 50%",
+        // toggleClass: "show",
+        onEnter: () => image.classList.add("show"),
+        markers: true,
+      },
+    });
+  });
+}
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
+  animationMaskCentral();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
