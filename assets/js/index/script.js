52310b12e8fa1de5b5d7164efb8887f8491e3563
Gary <hai.alphacreative@gmail.com>
2025-04-08 11:42:48 +0700

    add animation text

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -18,9 +18,37 @@ function animationMaskCentral() {
     });
   });
 }
+function animationText() {
+  const fxTitle = document.querySelectorAll("[data-splitting]");
+
+  fxTitle.forEach((element) => {
+    const chars = element.querySelectorAll(".char");
+
+    // Tạo timeline cho animation
+    const tl = gsap.timeline({ paused: true }).to(chars, {
+      color: "red",
+      stagger: 0.05, // Delay giữa các ký tự
+      duration: 0.2, // Thời gian đổi màu mỗi ký tự
+    });
+
+    // Hover events
+    element.addEventListener("mouseenter", () => {
+      tl.restart(); // Chạy animation khi hover
+    });
+
+    element.addEventListener("mouseleave", () => {
+      gsap.to(chars, {
+        color: "black",
+        stagger: 0.05,
+        duration: 0.2,
+      });
+    });
+  });
+}
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
   animationMaskCentral();
+  animationText();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
