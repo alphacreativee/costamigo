92052332a28143bf4f8bf872354f11ef5f75f65f
Gary <hai.alphacreative@gmail.com>
2025-04-10 11:53:35 +0700

    a

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -41,33 +41,34 @@ function sectionSlider() {
     const swiperContent = new Swiper(contentSwiperEl, {
       loop: false,
       effect: "fade",
-      allowTouchMove: false
+      allowTouchMove: false,
     });
 
     const swiperMain = new Swiper(mainSwiperEl, {
       loop: true,
       effect: "fade",
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
+      // autoplay: {
+      //   delay: 5000,
+      //   disableOnInteraction: false,
+      // },
       pagination: {
         el: paginationEl,
         clickable: true,
+        type: "fraction",
         renderBullet: function (i, className) {
           return `
             <button class="${className}">
               <svg class="progress" width="41" height="41"><circle class="circle-origin" r="20.5" cx="20.5" cy="20.5"></circle></svg>
               <span>${i + 1}</span>
             </button>`;
-        }
+        },
       },
       on: {
         slideChange: function () {
           const realIndex = this.realIndex;
           swiperContent.slideTo(realIndex);
-        }
-      }
+        },
+      },
     });
   });
 }
@@ -84,9 +85,9 @@ function imgWithText() {
     scrollTrigger: {
       trigger: ".image-with-text",
       start: "top top",
-      toggleActions: "play none none none"
+      toggleActions: "play none none none",
       // markers: true
-    }
+    },
   });
 
   gsap.to(".mask.bottom", {
@@ -96,9 +97,9 @@ function imgWithText() {
     scrollTrigger: {
       trigger: ".image-with-text",
       start: "top top",
-      toggleActions: "play none none none"
+      toggleActions: "play none none none",
       // markers: true
-    }
+    },
   });
 }
 
@@ -112,9 +113,9 @@ function animationMaskCentral() {
         start: "top 50%",
         end: "bottom 50%",
         // toggleClass: "show",
-        onEnter: () => image.classList.add("show")
+        onEnter: () => image.classList.add("show"),
         // markers: true
-      }
+      },
     });
   });
 }
@@ -138,7 +139,7 @@ function animationText() {
     const tl = gsap.timeline({ paused: true }).to(chars, {
       color: hoverColor, // Dùng màu từ CSS
       stagger: 0.05, // Delay giữa các ký tự
-      duration: 0.2 // Thời gian đổi màu mỗi ký tự
+      duration: 0.2, // Thời gian đổi màu mỗi ký tự
     });
 
     // Hover events
@@ -150,7 +151,7 @@ function animationText() {
       gsap.to(chars, {
         color: originalColor, // Trở về màu ban đầu từ CSS
         stagger: 0.05,
-        duration: 0.2
+        duration: 0.2,
       });
     });
   });
@@ -227,10 +228,11 @@ function animationText() {
   });
 }
 function swiperRestaurant() {
+  if (document.querySelector(".swiper-restaurant").length < 1) return;
   var swiperRes = new Swiper(".swiper-restaurant", {
     effect: "fade",
     slidesPerView: "auto",
-    centeredSlides: true
+    centeredSlides: true,
   });
   const contentRes = document.querySelectorAll(
     ".section-restaurant__content--title a"
