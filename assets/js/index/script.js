36097493845ed4c9647832ea00aa8340bae01fb2
Gary <hai.alphacreative@gmail.com>
2025-04-18 09:30:37 +0700

    a

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -50,7 +50,7 @@ function sectionSlider() {
     const swiperContent = new Swiper(contentSwiperEl, {
       loop: false,
       effect: "fade",
-      allowTouchMove: false
+      allowTouchMove: false,
     });
 
     const swiperMain = new Swiper(mainSwiperEl, {
@@ -58,19 +58,19 @@ function sectionSlider() {
       loop: true,
       navigation: {
         nextEl: swiperButton, // Gắn nút cho next
-        prevEl: swiperButton // Gắn nút cho prev
+        prevEl: swiperButton, // Gắn nút cho prev
       },
       pagination: {
         el: paginationEl,
         clickable: true,
-        type: "fraction"
+        type: "fraction",
       },
       on: {
         slideChange: function () {
           const realIndex = this.realIndex;
           swiperContent.slideTo(realIndex);
-        }
-      }
+        },
+      },
     });
 
     mainSwiperEl.addEventListener("mousemove", (e) => {
@@ -148,9 +148,9 @@ function imgWithText() {
       scrollTrigger: {
         trigger: section,
         start: "top 20%",
-        toggleActions: "play none none none"
+        toggleActions: "play none none none",
         // markers: true
-      }
+      },
     });
 
     gsap.to($(section).find(".mask.bottom"), {
@@ -160,8 +160,8 @@ function imgWithText() {
       scrollTrigger: {
         trigger: section,
         start: "top 20%",
-        toggleActions: "play none none none"
-      }
+        toggleActions: "play none none none",
+      },
     });
     gsap.to($(section).find(".content-box "), {
       yPercent: -10, // Move element upward by 20% of its height
@@ -170,8 +170,8 @@ function imgWithText() {
         trigger: $(section).find(".content-box"),
         start: "top 30%", // Start when the top of the image hits the bottom of the viewport
         end: "bottom top", // End when the bottom of the image hits the top of the viewport
-        scrub: true // Smoothly tie the animation to scroll
-      }
+        scrub: true, // Smoothly tie the animation to scroll
+      },
     });
   });
 }
@@ -186,9 +186,9 @@ function animationMaskCentral() {
         start: "top 70%",
         end: "bottom 70%",
         // toggleClass: "show",
-        onEnter: () => image.classList.add("show")
+        onEnter: () => image.classList.add("show"),
         // markers: true,
-      }
+      },
     });
   });
   const animationImagesBanner = document.querySelectorAll(
@@ -227,7 +227,7 @@ function animationText() {
     const tl = gsap.timeline({ paused: true }).to(chars, {
       color: hoverColor, // Dùng màu từ CSS
       stagger: 0.05, // Delay giữa các ký tự
-      duration: 0.2 // Thời gian đổi màu mỗi ký tự
+      duration: 0.2, // Thời gian đổi màu mỗi ký tự
     });
 
     // Hover events
@@ -239,7 +239,7 @@ function animationText() {
       gsap.to(chars, {
         color: originalColor, // Trở về màu ban đầu từ CSS
         stagger: 0.05,
-        duration: 0.2
+        duration: 0.2,
       });
     });
   });
@@ -250,7 +250,7 @@ function animationText() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         opacity: 1,
@@ -259,9 +259,9 @@ function animationText() {
         scrollTrigger: {
           trigger: element,
           start: "top 60%",
-          end: "bottom 60%"
+          end: "bottom 60%",
           // markers: true,
-        }
+        },
       }
     );
   });
@@ -272,7 +272,7 @@ function animationText() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         opacity: 1,
@@ -281,9 +281,9 @@ function animationText() {
         scrollTrigger: {
           trigger: element,
           start: "top 40%",
-          end: "bottom 40%"
+          end: "bottom 40%",
           // markers: true,
-        }
+        },
       }
     );
   });
@@ -295,19 +295,19 @@ function animationText() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         scrollTrigger: {
           trigger: element,
           start: "top 70%",
-          end: "bottom 70%"
+          end: "bottom 70%",
         },
         opacity: 1,
         y: 0,
         duration: 0.5,
         ease: "sine.out",
-        stagger: 0.1
+        stagger: 0.1,
       }
     );
   });
@@ -317,19 +317,19 @@ function animationText() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         scrollTrigger: {
           trigger: element,
-          start: "top 55%",
-          end: "bottom 55%"
+          start: "top 60%",
+          end: "bottom 60%",
         },
         opacity: 1,
         y: 0,
         duration: 0.5,
         ease: "sine.out",
-        stagger: 0.1
+        stagger: 0.1,
       }
     );
   });
@@ -339,7 +339,7 @@ function animationText() {
     let myDesc = new SplitType(element, {
       types: "lines, words",
       lineClass: "split-line",
-      wordClass: "split-word"
+      wordClass: "split-word",
     });
     myDesc.lines.forEach((line, index) => {
       gsap.from(line.querySelectorAll(".split-word"), {
@@ -351,9 +351,9 @@ function animationText() {
           trigger: element,
           start: "top center",
           end: "top center",
-          toggleActions: "play none none none"
+          toggleActions: "play none none none",
           // markers: true,
-        }
+        },
       });
     });
   });
@@ -362,7 +362,7 @@ function animationText() {
     let myDesc = new SplitType(element, {
       types: "lines, words",
       lineClass: "split-line",
-      wordClass: "split-word"
+      wordClass: "split-word",
     });
     myDesc.lines.forEach((line, index) => {
       gsap.from(line.querySelectorAll(".split-word"), {
@@ -374,9 +374,9 @@ function animationText() {
           trigger: element,
           start: "top 50%",
           end: "top 50%",
-          toggleActions: "play none none none"
+          toggleActions: "play none none none",
           // markers: true,
-        }
+        },
       });
     });
   });
@@ -386,7 +386,7 @@ function swiperRestaurant() {
   var swiperRes = new Swiper(".swiper-restaurant", {
     effect: "fade",
     slidesPerView: "auto",
-    centeredSlides: true
+    centeredSlides: true,
   });
   const contentRes = document.querySelectorAll(
     ".section-restaurant__content--title a"
@@ -422,8 +422,8 @@ function swiperAct() {
     pagination: {
       el: ".section-act__slider .swiper-pagination",
       clickable: true,
-      type: "fraction"
-    }
+      type: "fraction",
+    },
     // Không dùng navigation mặc định vì chúng ta sẽ tự xử lý
   });
 
@@ -516,8 +516,8 @@ function swiperOffer() {
     spaceBetween: 40,
     navigation: {
       nextEl: ".section-offer__slider .swiper-button-next",
-      prevEl: ".section-offer__slider .swiper-button-prev"
-    }
+      prevEl: ".section-offer__slider .swiper-button-prev",
+    },
   });
 }
 function animationLineMap() {
@@ -530,9 +530,9 @@ function animationLineMap() {
       trigger: ".svg-container",
       start: "top center",
       end: "bottom bottom",
-      scrub: true
+      scrub: true,
       // markers: true,
-    }
+    },
   });
   // gsap.to("#rect", {
   //   ease: "none",
@@ -575,7 +575,7 @@ function scrollHeader() {
       self.direction === 1
         ? $(".cta-group").addClass("hide")
         : $(".cta-group").removeClass("hide");
-    }
+    },
   });
 }
 
@@ -591,9 +591,9 @@ function animationArt() {
         trigger: image,
         start: "top bottom", // Start when the top of the image hits the bottom of the viewport
         end: "bottom top", // End when the bottom of the image hits the top of the viewport
-        scrub: true // Smoothly tie the animation to scroll
+        scrub: true, // Smoothly tie the animation to scroll
         // markers: false, // Set to true for debugging
-      }
+      },
     });
   });
 
@@ -623,8 +623,8 @@ function animationArt() {
         trigger: imageR,
         start: "top bottom",
         end: "bottom top",
-        scrub: true
-      }
+        scrub: true,
+      },
     });
   });
 }
@@ -639,7 +639,7 @@ function headerMenu() {
     y: 20,
     stagger: 0.1,
     duration: 0.3,
-    ease: "power2.out"
+    ease: "power2.out",
   });
 
   btnMenu.addEventListener("click", () => {
@@ -723,7 +723,7 @@ function magicCursor() {
 
   gsap.set(circle, {
     xPercent: -50,
-    yPercent: -50
+    yPercent: -50,
   });
 
   let mouseX = 0,
@@ -737,7 +737,7 @@ function magicCursor() {
     gsap.to(circle, {
       x: mouseX,
       y: mouseY,
-      duration: 0.1 // Không có độ trễ
+      duration: 0.1, // Không có độ trễ
     });
   });
 
